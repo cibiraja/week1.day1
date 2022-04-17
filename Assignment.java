d72fb1aa017b5837b6938262fa078489544a8a77
cibiraja <103836673+cibiraja@users.noreply.github.com>
2022-04-16 22:22:58 -1000

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,43 @@
+package week1.day1;
+
+public class Assignment {
+	public static void main(String[] args) {
+		int a=-40;
+		int b=0;
+		System.out.println(a<=b);
+		System.out.println("The number -40 is converted to 40");
+	
+	}
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
---
+++ Assignment2.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Assignment2 {
+	public static void main(String[] args) {
+		int num=35;
+		if(num>0) {
+			System.out.println("Number is positive");
+		}
+		if(num<0) {
+			System.out.println("Number is negative");
+		}
+		if(num==0) {
+			System.out.println("Number is neither positive nor negative");
+			
+		}
+				
+	}
+ 
+}
